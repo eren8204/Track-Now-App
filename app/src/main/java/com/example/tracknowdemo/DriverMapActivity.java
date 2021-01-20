3a5d1935b5afb92ddba5e8fdd279cb4466f6a094
Fahim Rahman <57598021+fahimrahman002@users.noreply.github.com>
2021-01-20 16:06:14 +0600

    Showing Current Location

---
+++ app/src/main/java/com/example/tracknowdemo/DriverMapActivity.java
@@ -0,0 +1,130 @@
+package com.example.tracknowdemo;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class DriverMapActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private static final String TAG = "DriverMapActivity";
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+    private static final float cameraZoom = 15f;
+    private Boolean myLocationPermissionsGranted = false;
+    private GoogleMap myMap;
+    private FusedLocationProviderClient myFusedLocationProviderClient;
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Toast.makeText(this, "Map is Ready", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onMapReady: map is ready");
+        myMap = googleMap;
+        if (myLocationPermissionsGranted) {
+            getDeviceLocation();
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+            }
+            myMap.setMyLocationEnabled(true);
+        }
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_map);
+        getLocationPermission();
+    }
+
+    private void initMap() {
+        Log.d(TAG, "initMap:initializing map");
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(DriverMapActivity.this);
+    }
+
+    private void getDeviceLocation() {
+        Log.d(TAG, "getDeviceLocation:getting the devices current location");
+        myFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        try {
+            if(myLocationPermissionsGranted){
+                Task location=myFusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if(task.isSuccessful()){
+                            Log.d(TAG,"onComplete: found location");
+                            Location currentLocation=(Location)task.getResult();
+                        moveCamera(new LatLng(currentLocation.getLatitude(),currentLocation.getLongitude()),cameraZoom);
+                        }else{
+                            Log.d(TAG,"onComplete: current location is null");
+                            Toast.makeText(DriverMapActivity.this,"Unable to get current location",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+
+        } catch (SecurityException e) {
+            Log.e(TAG, "getDeviceLocation: SecurityException " + e.getMessage());
+        }
+
+    }
+    private void moveCamera(LatLng latlng, float zoom){
+        Log.d(TAG,"moveCamera: moving the camera to: lat:"+latlng.latitude+", lan: "+latlng.longitude);
+        myMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng,zoom));
+
+    }
+
+    private void getLocationPermission() {
+        Log.d(TAG, "getLocationPermission: getting location permissions");
+        String[] permissions = {FINE_LOCATION, COARSE_LOCATION};
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(), COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                myLocationPermissionsGranted = true;
+                initMap();
+            } else {
+                ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        } else {
+            ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        myLocationPermissionsGranted = false;
+        switch (requestCode) {
+            case LOCATION_PERMISSION_REQUEST_CODE: {
+                if (grantResults.length > 0) {
+                    for (int i = 0; i < grantResults.length; i++) {
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                            myLocationPermissionsGranted = false;
+                            return;
+                        }
+                    }
+                    myLocationPermissionsGranted = true;
+                    initMap();
+                }
+            }
+        }
+    }
+}
