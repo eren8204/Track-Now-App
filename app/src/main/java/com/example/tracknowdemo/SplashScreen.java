d224cf83ec4f296d37b1689af6e7835758420dcd
Fahim Rahman <57598021+fahimrahman002@users.noreply.github.com>
2021-02-10 20:01:19 +0600

    login screen design complete

--- app/src/main/java/com/example/tracknowdemo/SplashScreen.java
+++ app/src/main/java/com/example/tracknowdemo/SplashScreen.java
@@ -2,9 +2,12 @@ package com.example.tracknowdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -39,9 +42,12 @@ public class SplashScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent=new Intent(SplashScreen.this,MainActivity.class);
-                startActivity(intent);
-                finish();
+                Intent intent=new Intent(SplashScreen.this,LoginActivity.class);
+                Pair[] pairs =new Pair[2];
+                pairs[0]=new Pair<View,String>(image,"logo_image");
+                pairs[1]=new Pair<View,String>(logo,"logo_text");
+                ActivityOptions options=ActivityOptions.makeSceneTransitionAnimation(SplashScreen.this,pairs);
+                startActivity(intent,options.toBundle());
             }
         },SPLASH_SCREEN);
     }
